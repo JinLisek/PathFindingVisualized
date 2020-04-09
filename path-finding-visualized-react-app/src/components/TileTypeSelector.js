414cd290393813bf126c8b23a8ffbed75632ceff
JinLisek <tastyramen@gmail.com>
2020-04-09 19:06:35 +0200

    Removed form from TileTypeSelector

--- path-finding-visualized-react-app/src/components/TileTypeSelector.js
+++ path-finding-visualized-react-app/src/components/TileTypeSelector.js
@@ -3,42 +3,40 @@ import React from "react";
 function TileTypeSelector() {
   return (
     <div className="container">
-      <form>
-        <div className="form-check">
-          <label>
-            <input
-              type="radio"
-              name="tile-type"
-              value="start"
-              defaultChecked
-              className="form-check-input"
-            ></input>
-            Start
-          </label>
-        </div>
-        <div className="form-check">
-          <label>
-            <input
-              type="radio"
-              name="tile-type"
-              value="end"
-              className="form-check-input"
-            ></input>
-            End
-          </label>
-        </div>
-        <div className="form-check">
-          <label>
-            <input
-              type="radio"
-              name="tile-type"
-              value="obstacle"
-              className="form-check-input"
-            ></input>
-            Obstacle
-          </label>
-        </div>
-      </form>
+      <div className="form-check">
+        <label>
+          <input
+            type="radio"
+            name="tile-type"
+            value="start"
+            defaultChecked
+            className="form-check-input"
+          ></input>
+          Start
+        </label>
+      </div>
+      <div className="form-check">
+        <label>
+          <input
+            type="radio"
+            name="tile-type"
+            value="end"
+            className="form-check-input"
+          ></input>
+          End
+        </label>
+      </div>
+      <div className="form-check">
+        <label>
+          <input
+            type="radio"
+            name="tile-type"
+            value="obstacle"
+            className="form-check-input"
+          ></input>
+          Obstacle
+        </label>
+      </div>
     </div>
   );
 }
